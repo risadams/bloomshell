933b78e5e1534915a786163218acf76296816a03
Ris Adams <ris@risadams.com>
2024-01-22 14:06:13 -0500

    Updated workflow and setup script
    The GitHub Actions workflow has been updated to include the checkout and installation of kcov. The dependencies installed by the workflow have also been expanded to include build-essential and cmake. The setup-action.sh script now includes steps for compiling and installing kcov, in addition to grabbing shellspec. The PATH is updated to include both shellspec and kcov.

--- setup-action.sh
+++ setup-action.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
+# grab shellspec
 curl -fsSL https://git.io/shellspec | sh -s -- --yes
 chmod +x $HOME/.local/bin/shellspec
-export PATH=$PATH:$HOME/.local/bin
-
 echo "Shellspec installed"
 shellspec -v
+
+# compile kcov, should be cloned via action
+cd $HOME/kcov
+cmake .
+make
+make install
+
+# path for both shellspac and kcov
+export PATH=$PATH:$HOME/.local/bin:/usr/local/bin
