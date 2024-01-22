6ea9a76d0c09d3662df1857c473f7092f924224a
Ris Adams <ris@risadams.com>
2024-01-22 16:23:41 -0500

    Added tests for source_lib function
    This commit introduces new tests for the 'source_lib' function. The tests cover three scenarios:
    - Loading shell libraries
    - Handling errors when a directory is missing
    - Ensuring that shell libraries are loaded only once.
    
    Additionally, a blank file has been added in the support directory for testing purposes.

---
+++ spec/core_spec.sh
@@ -0,0 +1,26 @@
+#shellcheck shell=bash
+
+Describe 'Evaluating source_lib mock: '
+  Include lib/core.sh
+
+  It 'Loads sh libs'
+    When call source_lib "./spec/support/libtest1"
+    The output should eq ''
+  End
+  
+  It 'lib error on missing dir'
+    When call source_lib "./spec/support/missing"
+    The output should eq 'Error: The directory ./spec/support/missing does not exist.'
+  End
+
+  load_dir_twice() {
+    source_lib "./spec/support/libtest1"
+    source_lib "./spec/support/libtest1"
+  }  
+
+  It 'Loads sh libs only once'
+    When call load_dir_twice
+    The output should eq ''
+  End
+
+End
