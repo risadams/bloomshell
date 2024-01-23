e10e11bcbeeba202a119b6cfce93b5f748f5b02a
Ris Adams <ris@risadams.com>
2024-01-23 07:36:54 -0500

    Refactor test setup in core_spec.sh
    The test setup and teardown methods have been simplified. The 'BeforeEach' and 'AfterEach' functions were replaced with a single 'BeforeAll' function, reducing the need to repeatedly change directories. Also, paths in the source_lib calls have been updated for better accuracy.

--- spec/core_spec.sh
+++ spec/core_spec.sh
@@ -2,35 +2,29 @@
 
 Describe 'Evaluating source_lib mock: '
   Include lib/core.sh
-  BeforeEach 'setup_working_directory'
-  AfterEach 'restore_directory'
+  BeforeAll 'setup_working_directory'
 
   # Use current path as default.
   # When running as a Github action, find the working directory.
-  work_dir="${GITHUB_WORKSPACE:-.}"
-
   setup_working_directory() {
-    original_dir=$(pwd)
+    local work_dir="${GITHUB_WORKSPACE:-.}"
     cd $work_dir
+    echo "Working in $work_dir"
   }
 
-  restore_directory() {
-    cd "$original_dir"
-  }  
-
   It 'Loads sh libs'
-    When call source_lib "./support/libtest1"
+    When call source_lib "./spec/support/libtest1"
     The output should eq ''
   End
   
   It 'lib error on missing dir'
-    When call source_lib "./support/missing"
-    The output should eq "Error: The directory ./support/missing does not exist."
+    When call source_lib "./spec/support/missing"
+    The output should eq "Error: The directory ./spec/support/missing does not exist."
   End
 
   load_dir_twice() {
-    source_lib "./support/libtest1"
-    source_lib "./support/libtest1"
+    source_lib "./spec/support/libtest1"
+    source_lib "./spec/support/libtest1"
   }  
 
   It 'Loads sh libs only once'
