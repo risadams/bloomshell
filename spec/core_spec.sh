e042a3fa80bfb78cebb4b244aeaca477d7c2ee39
Ris Adams <ris@risadams.com>
2024-01-23 07:40:41 -0500

    Refactored working directory setup in tests
    The working directory setup has been refactored for better flexibility. The 'work_dir' variable is now globally defined, allowing it to be used across different functions. This change ensures that the correct path is used when running as a Github action or locally. Also, all instances of source_lib have been updated to use this new 'work_dir' variable instead of hardcoded paths.

--- spec/core_spec.sh
+++ spec/core_spec.sh
@@ -6,25 +6,25 @@ Describe 'Evaluating source_lib mock: '
 
   # Use current path as default.
   # When running as a Github action, find the working directory.
+  work_dir="${GITHUB_WORKSPACE:-.}"
   setup_working_directory() {
-    local work_dir="${GITHUB_WORKSPACE:-.}"
     cd $work_dir
     echo "Working in $work_dir"
   }
 
   It 'Loads sh libs'
-    When call source_lib "./spec/support/libtest1"
+    When call source_lib "$work_dir/spec/support/libtest1"
     The output should eq ''
   End
   
   It 'lib error on missing dir'
-    When call source_lib "./spec/support/missing"
-    The output should eq "Error: The directory ./spec/support/missing does not exist."
+    When call source_lib "$work_dir/spec/support/missing"
+    The output should eq "Error: The directory $work_dir/spec/support/missing does not exist."
   End
 
   load_dir_twice() {
-    source_lib "./spec/support/libtest1"
-    source_lib "./spec/support/libtest1"
+    source_lib "$work_dir/spec/support/libtest1"
+    source_lib "$work_dir/spec/support/libtest1"
   }  
 
   It 'Loads sh libs only once'
