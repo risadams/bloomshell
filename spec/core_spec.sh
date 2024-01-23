9f5d71434418926045b2e1884d2c14a6891023e7
Ris Adams <ris@risadams.com>
2024-01-23 07:27:14 -0500

    Improved test environment setup
    Added setup and teardown functions to the core spec script. These functions change the working directory before each test and restore it afterwards, ensuring a consistent environment for each test run.

--- spec/core_spec.sh
+++ spec/core_spec.sh
@@ -2,24 +2,35 @@
 
 Describe 'Evaluating source_lib mock: '
   Include lib/core.sh
+  BeforeEach 'setup_working_directory'
+  AfterEach 'restore_directory'
 
   # Use current path as default.
   # When running as a Github action, find the working directory.
   work_dir="${GITHUB_WORKSPACE:-.}"
 
+  setup_working_directory() {
+    original_dir=$(pwd)
+    cd $work_dir
+  }
+
+  restore_directory() {
+    cd "$original_dir"
+  }  
+
   It 'Loads sh libs'
-    When call source_lib "$work_dir/spec/support/libtest1"
+    When call source_lib "./support/libtest1"
     The output should eq ''
   End
   
   It 'lib error on missing dir'
-    When call source_lib "$work_dir/spec/support/missing"
-    The output should eq "Error: The directory $work_dir/spec/support/missing does not exist."
+    When call source_lib "./support/missing"
+    The output should eq "Error: The directory ./support/missing does not exist."
   End
 
   load_dir_twice() {
-    source_lib "$work_dir/spec/support/libtest1"
-    source_lib "$work_dir/spec/support/libtest1"
+    source_lib "./support/libtest1"
+    source_lib "./support/libtest1"
   }  
 
   It 'Loads sh libs only once'
