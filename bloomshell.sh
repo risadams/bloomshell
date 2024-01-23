896c47204bfd75b30d1f0a9d85f51553fa848c9d
Ris Adams <ris@risadams.com>
2024-01-23 06:47:34 -0500

    Improved script setup and library loading
    The shell script has been updated to include the setup of global variables. This includes the definition of the script's root directory, which is then used for sourcing core library utilities. The error handling flag has been removed as well.

--- bloomshell.sh
+++ bloomshell.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-set -e
+# setup global variables
 VERSION="0.0.1"
 SCRIPT_NAME=$(basename "$0")
+SELF=$(readlink -f "$0")
+BLOOM_ROOT=${SELF%/*}
 
-source ./lib/core.sh
-source_lib ./lib
+# Load core library utilities
+source "$BLOOM_ROOT/lib/core.sh"
+source_lib "$BLOOM_ROOT/lib"
