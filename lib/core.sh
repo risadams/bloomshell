5b1c16668d3c7763571eee9e22eea284f6b6667d
Ris Adams <ris@risadams.com>
2024-01-24 07:40:34 -0500

    Enhanced core.sh with additional checks and features
    The core.sh script has been significantly updated. New checks have been added to prevent non-bloom execution and multiple sourcing. The script now also declares arrays for libraries, commands, and plugins. A new function `is_plugin` has been introduced to check if a plugin exists.
    
    The `__source` function was created to source files based on their context (library, command or plugin). It also adds the sourced file's context to the respective array.
    
    The `source_lib` function documentation was reformatted for better readability. The function itself now uses the new `__source` method instead of directly sourcing files.
    
    Finally, all libraries are loaded at once using the `source_lib` function. If any plugins exist, they are sourced as well. The loaded libraries, commands, and plugins are then exported for use in other scripts.

--- lib/core.sh
+++ lib/core.sh
@@ -17,42 +17,85 @@
 # - It is required for basic functionality
 # ------------------------------------------------------------------------------
 
+# Protect against running outside of bloom execution.
+if [ -z "$BLOOMSH_VERSION" ]; then
+  echo "This must be executed from a bloom session."
+  exit 1
+fi
+
+# Protect against sourcing multiple times
 if [ -n "$_BLOOMSH_CORE_SOURCED" ]; then
   return 0
 fi
-
 _BLOOMSH_CORE_SOURCED=1
+
+declare -a BLOOMSH_LIBS
+declare -a BLOOMSH_CMDS
+declare -a BLOOMSH_PLUGINS
+
+is_plugin() {
+  local base_dir=$1
+  local name=$2
+  test -f "$base_dir/plugins/$name/$name.plugin.sh" ||
+    test -f "$base_dir/plugins/$name/_$name"
+}
+
+__source() {
+  local context filepath="$1"
+
+  # Construct context based on path
+  case "$filepath" in
+  lib/*) # lib_name from lib/lib_name.sh
+    context="lib:$(basename "${filepath%.*}")"
+    BLOOMSH_LIBS+=($context)
+    ;;
+  commands/*) # commands_name from commands/command_name.sh
+    context="commands:$(basename "${filepath%.*}")"
+    BLOOMSH_CMDS+=($context)
+    ;;
+  plugins/*) # plugin_name from plugins/plugin_name/file.sh
+    context="plugins:$(basename "$(dirname "$filepath")")"
+    BLOOMSH_PLUGINS+=($context)
+    ;;
+  esac
+
+  # Disable shellcheck complaining about variables in sourced files
+  # shellcheck disable=SC1090
+  if [[ -f "$BLOOM_ROOT/$filepath" && -r "$BLOOM_ROOT/$filepath" ]]; then
+    source "$BLOOM_ROOT/$filepath"
+  fi
+}
 
 source_lib() {
   : <<'DOC'
- NAME
-        source_lib - Sources shell script files from a specified directory.
-    SYNOPSIS
-        source_lib [DIRECTORY]
-    DESCRIPTION
-        source_lib is a function that takes a single argument, a directory path,
-        and sources all shell script files (.sh) located within that directory.
-        
-        This function first checks if the specified directory exists. If it does,
-        it iterates over each shell script file in the directory. For each file,
-        it checks if the file exists and is readable. If both conditions are met,
-        the script file is sourced, and a confirmation message is echoed to
-        the terminal, indicating the file has been sourced.
+NAME
+    source_lib - Sources shell script files from a specified directory.
+SYNOPSIS
+    source_lib [DIRECTORY]
+DESCRIPTION
+    source_lib is a function that takes a single argument, a directory path,
+    and sources all shell script files (.sh) located within that directory.
+    
+    This function first checks if the specified directory exists. If it does,
+    it iterates over each shell script file in the directory. For each file,
+    it checks if the file exists and is readable. If both conditions are met,
+    the script file is sourced, and a confirmation message is echoed to
+    the terminal, indicating the file has been sourced.
 
-        If the specified directory does not exist, an error message is displayed.
-    ARGUMENTS
-        DIRECTORY
-            A path to the directory containing shell script files to be sourced.
-            The directory must exist and contain files with a .sh extension.
-    OUTPUT
-        For each sourced script file, a confirmation message "Sourced: [FILE_PATH]"
-        is displayed. If the directory does not exist, an error message is shown.
-    EXAMPLES
-        source_lib "/path/to/scripts"
-            This will source all .sh files located in '/path/to/scripts'.
-    NOTES
-        This function does not recursively source files in subdirectories.
-        It only sources files directly within the specified directory.
+    If the specified directory does not exist, an error message is displayed.
+ARGUMENTS
+    DIRECTORY
+        A path to the directory containing shell script files to be sourced.
+        The directory must exist and contain files with a .sh extension.
+OUTPUT
+    For each sourced script file, a confirmation message "Sourced: [FILE_PATH]"
+    is displayed. If the directory does not exist, an error message is shown.
+EXAMPLES
+    source_lib "/path/to/scripts"
+        This will source all .sh files located in '/path/to/scripts'.
+NOTES
+    This function, intentionally, does not recursively source files in subdirectories.
+    It only sources files directly within the specified directory.
 DOC
   local dir="$1"
 
@@ -63,10 +106,36 @@ DOC
       if [ -f "$script_file" ] && [ -r "$script_file" ]; then
         # Disable shellcheck complaining about variables in sourced files
         # shellcheck disable=SC1090
-        source "$script_file"
+        __source "lib/$(basename "$script_file")"
       fi
     done
   else
     echo "Error: The directory $dir does not exist."
   fi
 }
+
+# Load all libs
+source_lib "$BLOOM_ROOT/lib"
+
+# are there any plugins?
+for plugin in ${plugins[@]}; do
+  __source "plugins/$plugin/$plugin.plugin.sh"
+done
+unset plugin
+
+# Export the loaded libs,cmds,plugins for use in other scripts
+BLOOMSH_LIBS_STRING=$(
+  IFS=,
+  echo "${BLOOMSH_LIBS[*]}"
+)
+BLOOMSH_CMDS_STRING=$(
+  IFS=,
+  echo "${BLOOMSH_CMDS[*]}"
+)
+BLOOMSH_PLUGINS_STRING=$(
+  IFS=,
+  echo "${BLOOMSH_PLUGINS[*]}"
+)
+export BLOOMSH_LIBS_STRING
+export BLOOMSH_CMDS_STRING
+export BLOOMSH_PLUGINS_STRING
